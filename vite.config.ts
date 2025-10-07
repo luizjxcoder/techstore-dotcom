26e6bff43e39c9e1fd4a98cdc80b31d2513341a4
Luiz Alberto Jr. <64710056+luizjxcoder@users.noreply.github.com>
2025-10-07 16:48:54 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
+  base: "/techstore-dotcom/",
   server: {
-    host: "::",
-    port: 8080,
+    // server options here (if needed)
   },
   plugins: [
     react(),
